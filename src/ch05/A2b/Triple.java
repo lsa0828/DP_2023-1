ee57a66bab29470e009dd6bb5db2fa8b7edb4b1f
lsa0828 <lsa0828@duksung.ac.kr>
2023-04-04 11:44:39 +0900

    4월4일수업내용

--- src/ch05/A2b/Triple.java
+++ src/ch05/A2b/Triple.java
@@ -1,31 +1,13 @@
-package ch05.A2a;
+package ch05.A2b;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+public enum Triple {
+    ALPHA, BETA, GAMMA;
 
-public class Triple {
-    private static Map<String,Triple> map = new HashMap<>();
-        // 클래스 로드 시 실행할 코드 => 해시맵을 완성함(key: 이름, value: Triple 객체)
-    static {
-        String[] names = { "ALPHA", "BETA", "GAMMA" };
-        // 스트림을 생성 => 각 원소에 대해 => 해시맴에 넣어라
-        Arrays.stream(names).forEach(s -> map.put(s, new Triple(s)));
-    }
-
-    private String name;
-
-    private Triple(String name) {
-        System.out.println("The instance " + name + " is created.");
-        this.name = name;
+    private Triple() {
+        System.out.println("The instance " + this + " is created.");
     }
 
     public static Triple getInstance(String name) {
-        return map.get(name); // 해시맵에서 해당 이름의 Triple 객체를 얻어와서 반환함
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
+        return valueOf(name);
     }
 }
