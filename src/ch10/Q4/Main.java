94705235f4a4c3794b2c451a004d236e104f51ee
lsa0828 <lsa0828@duksung.ac.kr>
2023-04-13 09:27:40 +0900

    4월13일수업내용

--- src/ch10/Q4/Main.java
+++ src/ch10/Q4/Main.java
@@ -8,9 +8,11 @@ class Main {
         List<String> list = Arrays.asList("D", "B", "C", "E", "A");
 
         // 사전순으로 작은 순서(오름차순)
+        // list: 전략을 이용하는 컨텍스트
+        // Comparator: 전략 객체
         list.sort(new ComparatorAscending());
         System.out.println(list);
-
+ 
         // 사전순으로 큰 순서(내림차순)
         list.sort(new ComparatorDeseding());
         System.out.println(list);
