1392a19d0fd6f7b2c363ec2d6a7c3e16004e25d1
lsa0828 <lsa0828@duksung.ac.kr>
2023-05-25 10:16:05 +0900

    5월25일수업내용

--- src/ch15/Sample/Main.java
+++ src/ch15/Sample/Main.java
@@ -4,6 +4,8 @@ import ch15.Sample.pagemaker.PageMaker;
 
 public class Main {
     public static void main(String[] args) {
+        // Facade를 이용함
         PageMaker.makeWelcomePage("hyuki@example.com", "welcome.html");
+        PageMaker.makeWelcomePage("csh@duksung.ac.kr", "welcome2.html");
     }
 }
